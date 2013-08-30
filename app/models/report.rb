9e31665dbeb2d30a8718c44bd73db9b234881051
Terarecon Computer <tashfeen.ekram@gmail.com>
2013-08-29 17:35:24 -0700

    bootrapped

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,13 @@
 class Report < ActiveRecord::Base
-  attr_accessible :accession_number, :attending_id, :certainty, :department, :dob, :exam_code, :exam_date, :impression, :positivity, :pseudo_mrn, :report, :requesting_md_id
+	
+	attr_accessible :accession_number, :attending_id, :certainty, :department, :dob, :exam_code, :exam_date, :impression, :positivity, :pseudo_mrn, :report, :requesting_md_id
+	
+	def self.import()
+		CSV.foreach("ctpe.csv", {:col_sep => "|"}) do |row|
+			puts row[0]
+		end
+	end
+
+
+
 end
